9bb820fa973329c0df41d083381091705bfbf4f9
ohjuntaek <201211734@pukyong.ac.kr>
2020-06-29 23:50:37 +0900

    jwp 계산기 내가한거

---
+++ lab-jwp-calculator/src/main/java/view/InputView.java
@@ -0,0 +1,10 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static String 입력받기() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
---
+++ lab-jwp-calculator/src/test/java/MainTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    void hi() {
+        System.out.println("hi?");
+    }
+}
