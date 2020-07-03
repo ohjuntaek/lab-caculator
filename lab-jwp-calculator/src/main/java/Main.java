766d649cb9775422ea9a9eacf24369814fb5a226
ohjuntaek <201211734@pukyong.ac.kr>
2020-07-03 21:42:25 +0900

    책에 나온대로

--- lab-jwp-calculator/src/main/java/Main.java
+++ lab-jwp-calculator/src/main/java/Main.java
@@ -1,9 +1,9 @@
-import domain.계산기;
+import domain.StringCalculator;
 import view.InputView;
 
 public class Main {
     public static void main(String[] args) {
         String 입력받은문자열 = InputView.입력받기();
-        계산기 계산기1 = new 계산기(입력받은문자열);
+        StringCalculator StringCalculator1 = new StringCalculator(입력받은문자열);
     }
 }
